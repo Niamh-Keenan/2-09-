0ce303277e1fa7866a85e006be21e1efea0f572b
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-10-10 23:13:37 +0000

    Debugged

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 age=int(input("Enter your age:"))
 
-if age>=17:
-  print("You can see an R rated movie by yourself.");
+if age >= 17:
+ print("You can see an R rated movie by yourself.")
   
-else (age>=13)
-  print("You can see a PG-13 movie alone.");
+elif age >= 13:
+  print("You can see a PG-13 movie alone.")
 
-else if (age>=5)
-  print("You can see a G or PG movie alone.")
+elif age >= 5:
+  print ("You are too young for most things.")
+
+else: 
+  print("You are too young for most things.")
