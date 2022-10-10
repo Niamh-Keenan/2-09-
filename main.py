4bd5aa8eba291d93a21cb81564b9018169f53fc4
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-10-10 21:19:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age:"))
+
+if age>=17:
+  print("You can see an R rated movie by yourself.");
+  
+else (age>=13)
+  print("You can see a PG-13 movie alone.");
+
+else if (age>=5)
+  print("You can see a G or PG movie alone.")
